a2b4e1d1d8bd146aa6e941a4aa255664e08bdf70
Olha Yarosh <olha.yarosh1@gmail.com>
2022-07-31 18:33:04 +0300

    Added top toggle buttons.

--- src/components/ButtonRegister.js
+++ src/components/ButtonRegister.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import classes from './ButtonRegister.module.css'
 
 const ButtonRegister = () => {
     return (
-        <>
-        </>
+        <div className={classes.buttonBox}>
+                <button type='button' className={classes.toggleBtnLogin} onclick='login()'>LOGIN</button>
+                <button type='button' className={classes.toggleBtnRegister} onclick='register()'>REGISTER</button>
+        </div>
     )
 }
 

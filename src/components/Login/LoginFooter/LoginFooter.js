51dfde3e80dd8661a2711a15252b00d031b9503d
Olha Yarosh <olha.yarosh1@gmail.com>
2022-08-03 21:57:39 +0300

    Code style.

--- src/components/Login/LoginFooter/LoginFooter.js
+++ src/components/Login/LoginFooter/LoginFooter.js
@@ -4,18 +4,18 @@ import classes from './LoginFooter.module.css'
 const LoginFooter = () => {
     return (
         <>
-                <input type='checkbox' className={classes.checkBox}/>
-                <div className={classes.circle}></div>
-                <div className={classes.forgotPassword}>
-                    <a href='/'>Forgot password?</a>
-                    <div className={classes.label}>Remember me</div>
-                </div>
-                <footer>
-                    <input type='submit' value='SIGN IN' className={classes.btn}/>
-                    <p>Not a member?&nbsp;
-                        <a href='/Register'>Register</a>
-                    </p>
-                </footer>
+            <input type='checkbox' className={classes.checkBox}/>
+            <div className={classes.circle}></div>
+            <div className={classes.forgotPassword}>
+                <a href='/'>Forgot password?</a>
+                <div className={classes.label}>Remember me</div>
+            </div>
+            <footer>
+                <input type='submit' value='SIGN IN' className={classes.btn}/>
+                <p>Not a member?&nbsp;
+                    <a href='/Register'>Register</a>
+                </p>
+            </footer>
         </>
     )
 }

00d0488be243d3917aa5efed98a82821a5ee17b6
Olha Yarosh <olha.yarosh1@gmail.com>
2022-08-03 21:55:52 +0300

    Added a circle around the register checkbox.

--- src/components/Register/RegisterFooter/RegisterFooter.js
+++ src/components/Register/RegisterFooter/RegisterFooter.js
@@ -4,10 +4,9 @@ import classes from './RegisterFooter.module.css'
 const CheckboxRegister = () => {
     return (
         <>
-            <div className={classes.formControlCheckReg}>
-                <input type='checkbox'/>
-                <label>I have read and agree to the terms</label>
-            </div>
+            <input className={classes.checkbox} type='checkbox'/>
+            <div className={classes.circle}></div>
+            <div className={classes.label}>I have read and agree to the terms</div>
             <input type='submit' value='SIGN IN' className={classes.btn}/>
         </>
     )

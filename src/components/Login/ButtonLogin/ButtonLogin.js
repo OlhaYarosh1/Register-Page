46f943ea4678f61750f800ba6b36e1874fce4e2c
Olha Yarosh <olha.yarosh1@gmail.com>
2022-08-04 14:49:38 +0300

    Added navigation between pages using buttons.

--- src/components/Login/ButtonLogin/ButtonLogin.js
+++ src/components/Login/ButtonLogin/ButtonLogin.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import classes from './ButtonLogin.module.css'
 
+
 const ButtonLogin = () => {
+    function gotoLink(link) {
+        console.log(link.value);
+        window.open = link.value;
+    }
     return (
         <div className={classes.buttonBox}>
             <button type='button' className={classes.toggleBtnLogin} autoFocus>
                 LOGIN
             </button>
+            <a href='http://localhost:3000/Register'>
             <button type='button' className={classes.toggleBtnRegister}>
                 REGISTER
             </button>
+            </a>
         </div>
     )
 }
--- src/components/Register/ButtonRegister/ButtonRegister.js
+++ src/components/Register/ButtonRegister/ButtonRegister.js
@@ -4,9 +4,11 @@ import classes from '../../Login/ButtonLogin/ButtonLogin.module.css'
 const ButtonRegister = () => {
     return (
         <div className={classes.buttonBox}>
+            <a href='http://localhost:3000'>
             <button type='button' className={classes.toggleBtnLogin}>
                 LOGIN
             </button>
+            </a>
             <button type='button' className={classes.toggleBtnRegister} autoFocus>
                 REGISTER
             </button>
